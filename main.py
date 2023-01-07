21c5c5102d100297aadc1b35da0f1dff546a1d58
Amin05th <amindhahri16@gmail.com>
2023-01-07 10:12:03 +0100

    scraped more data

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from data_scraper import get_warzone_stats
+from name_scraper import fetch_names
 
 user = ['LOST_Beniboy_XD', 'Ghost_Balor1916', 'ghost_danjel', 'AmirBacha19', 'IV_40_kirtap']
 
-df = get_warzone_stats(user)
+names_list = fetch_names("./chromedriver", 1)
+names_list.extend(user)
+print(names_list)
+df = get_warzone_stats(names_list)
 df.to_csv("warzonestats.csv")
+
